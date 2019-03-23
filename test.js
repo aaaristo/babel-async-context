7f66c0a04c220b4495979da5189cf31cfb1db90b
Andrea Gariboldi <gariboldi@lifestage-solutions.ch>
2019-03-23 14:01:38 +0100

    initial

---
+++ test.js
@@ -0,0 +1,20 @@
+async function bar(p) {
+  console.log("bar");
+  const ctx = await getAsyncContext();
+  console.log("bar2");
+  console.log(p + " " + ctx.my);
+}
+
+async function foo() {
+  console.log("foo");
+  const x = bar("1");
+  await bar("2");
+  await x;
+  await Promise.all([bar("4"), bar("5")]);
+}
+
+(async () => {
+  const ctx = await getAsyncContext();
+  ctx.my = "ciao";
+  await foo();
+})().catch(x => console.log(x));
